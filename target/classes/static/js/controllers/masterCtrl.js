3f386b88466a906a99295084cba0f3337e3c49f4
Rafug <fafson97@gmail.com>
2019-02-17 12:16:10 +0100

    e

--- target/classes/static/js/controllers/masterCtrl.js
+++ target/classes/static/js/controllers/masterCtrl.js
@@ -7,22 +7,122 @@ angular.module('myApp.controllers')
         ['$rootScope','$scope', '$http', '$timeout', '$interval', '$q',
             function ($rootScope, $scope, $http, $timeout, $interval, $q ) {
 
+
                 $scope.M = {};
                 $scope.logins=false;
                 $scope.pass=false;
                 $scope.M.tsp = [];
+                $scope.enginestatus = {};
+                const URL ="http://localhost:8445"
+
 
                 //noinspection JSAnnotator,JSAnnotator
-                $http.get('http://localhost:8445/jd')
-                    .then(function(response) {
-                        $scope.myTest = response.data;
-                    });
+                $scope.xx = function(){
+                    $http.get('http://localhost:8445/test')
+                        .then(function(response) {
+                            console.log('XXXXX' + response.resp);
+                            $scope.myTest = response.data;
+                        });
 
-                $http.get($rootScope.M.URL + '/temp?id=1')
-                    .then(function(response) {
-                        $scope.myTest2 = response.data;
+
+
+
+                };
+
+
+
+                let callEngine = function (arg) {
+                    $http({
+                        url: URL +'/'+ arg,
+                        method: 'GET',
+                        params: {
+
+                        }
+                    }).success(function (dane) {
+                        $scope.enginestatus = dane;
                     });
+                };
+
+                $scope.startEngine = function() {
+                    const nr = document.getElementById("ip2");
+                    console.log("zapisuję logi: " + nr.value);
+                    if(nr.value<2000){}
+                    $http({
+                        url: $rootScope.M.URL + '/freqs?freq=' + nr.value,
+                        method: 'GET',
+                    })
+
+
+
+
+                    ;
+                    callEngine("start");
+                };
+
+
+
+                $scope.stopEngine = function(){
+
+                    callEngine("stop");
+                };
+
+
+                $scope.loadEngineState =function () {
+                    callEngine("status");
+                };
+
+                $scope.reverseEngine = function () {
+                    callEngine("reverse");
+                };
+
+                $scope.loadEngineState();
+
+
+
+
+
+                $scope.jd = function () {
+                    $http.get($rootScope.M.URL + '/temp?id=1')
+                        .then(function(response) {
+                            $scope.myTest2 = response.data;
+                        });
+                };
+
+
+
+                this.postForm = function() {
+                    var encodedString = 'username=' +
+                        encodeURIComponent(this.inputData.username) +
+                        '&password=' +
+                        encodeURIComponent(this.inputData.password);
+
+                    $http({
+                        method: 'POST',
+                        url: 'userauth.php',
+                        data: encodedString,
+                        headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                    })
+
+                        .success(function(data) {
+                            console.log(data);
+                            if ( data.trim() === 'correct') {
+                                window.open("http://localhost:8445/test.html");
+                            } else {
+                                alert("Proszę wprowadzić poprawny login i hasło");
+                            }
+                        })
+                }
+
+
+
+
+
+
 
             }
         ]
-    );
+    )
+
+.controller('loginCtrl'[ function($scope, $http, $location, user) {
+
+}]);
--- target/classes/static/js/mainApp.js
+++ target/classes/static/js/mainApp.js
@@ -4,7 +4,6 @@ var app = angular.module('myApp', [
     'ngRoute',
     'myApp.controllers'
 ]);
-
 app.config(['$routeProvider', function ($routeProvider) {
     const urlBase = 'partials/';
 
@@ -15,14 +14,20 @@ app.config(['$routeProvider', function ($routeProvider) {
       .when('/view1', {
         templateUrl: urlBase + 'sterowanieView.html',
         controller: 'masterCtrl'
+
     }).when('/view2', {
-        templateUrl: urlBase + 'monitoringView.html',
+        templateUrl: urlBase + 'monioringView.html',
         controller: 'masterCtrl'
     })
     .when('/view3', {
         templateUrl: urlBase + 'homeView.html',
         controller: 'masterCtrl'
     })
+        .when('/view4',{
+            templateUrl:urlBase + 'projectView.html',
+            controller: 'masterCtrl'
+        })
+
     ;
 }]);
 
@@ -31,5 +36,8 @@ app.run(function ($rootScope) {
 
     $rootScope.M = {};
     //Global properties
-    $rootScope.M.URL = 'http://localhost:8445';
+    $rootScope.M.URL = 'localhost:8445';
 });
+
+
+
